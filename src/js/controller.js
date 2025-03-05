13b2bfb06cb60e37717cd267ae7a96b4461fab6c
MaryKaratu <Karatumary85@gmail.com>
2025-03-05 14:43:55 +0100

    Updating Recipe Servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -59,8 +59,17 @@ const controlPagination = function (gotoPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  //Update the recipe servings (in the state)
+  model.updateServings(newServings);
+
+  //Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- src/js/model.js
+++ src/js/model.js
@@ -65,3 +65,11 @@ export const getSearchResultsPage = function (page = state.search.page) {
   const end = page * state.search.resultPerPage; //9;
   return state.search.results.slice(start, end);
 };
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings; // newQ = oldQt * newServings / oldServings //2 *8 / 4 =4
+
+    state.recipe.servings = newServings;
+  });
+};
